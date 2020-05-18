a580bd4a6ec4ecc24924e5c9446a8d6106954d3e
WeCodeMakeCode <52891037+WeCodeMakeCode@users.noreply.github.com>
2020-05-18 13:28:58 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,49 @@
 namespace SpriteKind {
     export const circle = SpriteKind.create()
 }
-class circle{
-    public radius:number
-    public color:number
-    private _circle_image:Image
-    private _circle_sprite:Sprite
-    private _wh:number
+class circle {
+    private radius:number
+    private color:number
+    private circle_image:Image
+    private circle_sprite:Sprite
+    private wh:number
     constructor(radius:number, color:number, 
-                x:number = screen.width/2, 
-                y:number=screen.height/2){
+                x:number = 80, 
+                y:number= 40){
         this.radius = radius
         this.color = color
-        this._wh = this.radius/2
-        this._circle_image = image.create(this._wh,this._wh)
-        this._circle_image.drawCircle(x, y, this.radius, this.color)
-        this._circle_sprite = sprites.create(this._circle_image,SpriteKind.circle)
+        this.wh = 2 * this.radius + 1
+        this.circle_image = image.create(this.wh,this.wh)
+        this.circle_image.drawCircle(this.radius, this.radius, this.radius, this.color)
+        this.circle_sprite = sprites.create(this.circle_image,SpriteKind.circle)
+        this.circle_sprite.x = x
+        this.circle_sprite.y = y
     }
+    getSprite():Sprite{
+        return this.circle_sprite
+    }
+    getImage(): Image {
+        return this.circle_image
+    }
+
 }
+function add_circle():circle{
+    radius = 2 * Math.randomRange(10, 30) + 1
+    color_number = (color_number + 1) % 12
+    let c = new circle(radius, color_number)
+    let cs = c.getSprite()
+    cs.vx = Math.randomRange(-100, 100)
+    cs.vy = Math.randomRange(-100, 100)
+    cs.setFlag(SpriteFlag.BounceOnWall, true)
+    return c;
+}
+
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    let c:circle = add_circle()
+})
+controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
+    let c: circle = add_circle()
+})
+let color_number = -1
+let radius = 0
+let c:circle = add_circle()
